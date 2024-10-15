13107c8c3f1f772b0da1d9f48a56226b267765e3
AndrewMatanari <www.andrewmatanari@gmail.com>
2024-10-15 17:10:26 +0700

    Final

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Pokemon') }}</title>
+    <title>UTS PBWL - 2022130035</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
